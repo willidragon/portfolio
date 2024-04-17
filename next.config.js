cdfc486e8843e59c54c00413078bc5e898c7beca
willidragon <willidragon@gmail.com>
2024-04-18 02:09:25 +0800

    added config options for deployemnt vs prod

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production"; // This checks if the build is for production
 const nextConfig = {
+  basePath: isProd ? "/portfolio" : "",
+  assetPrefix: isProd ? "/portfolio/" : "",
   output: "export",
 };
 
